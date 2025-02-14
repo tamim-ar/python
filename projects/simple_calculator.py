de35fc8a727a6de8e2cb4090518bda573e7a21cc
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-02-14 19:58:05 +0600

    simple_calculator

---
+++ projects/simple_calculator.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum_ = a + b
+product = a * b
+difference = a - b
+quotient = a / b
+
+print(f"Sum: {sum_}")
+print(f"Product: {product}")
+print(f"Difference: {difference}")
+print(f"Quotient: {quotient}")
